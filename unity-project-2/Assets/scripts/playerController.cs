f49b0517896076e2fa5a4a7fb5dbec30906753a7
DrewAlden <130063470+DrewAlden@users.noreply.github.com>
2023-04-22 18:36:32 -0400

    Update playerController.cs
    /************************************************************
     * HOW TO USE:
     * 
     * All cheese pieces must have the "pickup" tag
     * The end hole must be a physical object with the "hole" tag
     * The cat must have the "Enemy" tag
     * 
     * ----------------------------------------------------------
     * 
     * For the script to work properly, I need Tiberius to add
     * the script for changing scenes and for the player's
     * position reset when they touch the cat
     * 
     * Speed will need to be edited within the script iself; I
     * did that for consistency's sake, so the speed isn't
     * different across different levels
     * 
     ************************************************************/

--- unity-project-2/Assets/scripts/playerController.cs
+++ unity-project-2/Assets/scripts/playerController.cs
@@ -1,3 +1,25 @@
+/************************************************************
+ * HOW TO USE:
+ * 
+ * All cheese pieces must have the "pickup" tag
+ * The end hole must be a physical object with the "hole" tag
+ * The cat must have the "Enemy" tag
+ * 
+ * ----------------------------------------------------------
+ * 
+ * For the script to work properly, I need Tiberius to add
+ * the script for changing scenes and for the player's
+ * position reset when they touch the cat
+ * 
+ * Speed will need to be edited within the script iself; I
+ * did that for consistency's sake, so the speed isn't
+ * different across different levels
+ * 
+ ************************************************************/
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +28,21 @@ using TMPro;
 
 public class playerController : MonoBehaviour
 {
-    public float speed = 0;
+    public float speed;
+    public int cheeseGoal;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
-
+    private Vector3 playerStartPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerStartPos = transform.position;
+        speed = 10;
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
@@ -36,7 +61,7 @@ public class playerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if (count >= cheeseGoal)
         {
             winTextObject.SetActive(true);
         }
@@ -50,12 +75,24 @@ public class playerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("pickup"))
+        if (other.gameObject.CompareTag("pickup"))
         {
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
         }
+        else if (other.gameObject.CompareTag("hole") && count >= cheeseGoal) //If the player touches the hole after getting all of the cheese
+        {
+            //TIBERIUS, THE END LEVEL SCRIPT GOES HERE*************************************************************************************************************************************
+        }
+
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            transform.position = playerStartPos;
+        }
+    }
 }
