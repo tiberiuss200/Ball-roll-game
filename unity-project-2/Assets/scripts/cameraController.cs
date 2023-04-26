fbd7956b3a3e6040a0d694e495f8708027b940ee
DrewAlden <130063470+DrewAlden@users.noreply.github.com>
2023-04-26 14:07:40 -0400

    Update cameraController.cs

--- unity-project-2/Assets/scripts/cameraController.cs
+++ unity-project-2/Assets/scripts/cameraController.cs
@@ -9,23 +9,26 @@ public class cameraController : MonoBehaviour
     public float turnSpd;
     public float cameraTilt;
     private Vector3 playerMovementDirection;
+    private Vector3 offset;
     Rigidbody rb;
+    // Start is called before the first frame update
     void Start()
     {
         cameraDistance = 4f;
-        turnSpd = 5;
-        cameraTilt = 1.5f;
+        turnSpd = 3;
+        cameraTilt = 0.6f;
         rb = player.GetComponent<Rigidbody>();
     }
+
+    // Update is called once per frame
     void LateUpdate()
     {
         playerMovementDirection = rb.velocity;
-        if(rb.velocity.magnitude > 1)
+        if(player.GetComponent<playerController>().sideInput != 0)
         {
             Quaternion rotation = Quaternion.LookRotation(-Vector3.up + cameraTilt * playerMovementDirection.normalized, playerMovementDirection);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * turnSpd);
         }
-
-        transform.position = player.transform.position - transform.forward * 7f;
+        transform.position = player.transform.position - transform.forward*cameraDistance;
     }
 }
